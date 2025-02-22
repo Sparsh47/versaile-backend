b8ff60f9261706d6b6aa0f55749c021fe8cee41a
Sparsh47 <sparshshandilya123@gmail.com>
2025-02-22 17:39:14 +0530

    removed rate limiting from auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,7 @@
 import express from "express";
 import {login, signup} from "../controllers/user.controller.js";
-import {authRateLimiter} from "../middlewares/authRateLimiter.js";
 
 export const router = express.Router();
 
-router.post("/signup", authRateLimiter, signup)
-router.post("/login", authRateLimiter, login)
+router.post("/signup", signup)
+router.post("/login", login)
